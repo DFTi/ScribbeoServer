499c696bb5a17859b2372aea99828506df33f686
Keyvan <keyvan@brevity>
2012-01-31 15:00:53 -0800

    create a tarball

---
+++ tarball.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Code/ScribbeoServer ~/Desktop/ScribbeoServer-Src-1.0.tar.gz
+cp -r ~/Projects/ScribbeoServer ~/Code/ScribbeoServer
+rm -rf ~/Code/ScribbeoServer/.git
+tar -czf ~/Desktop/ScribbeoServer-Src-1.0.tar.gz ~/Code/ScribbeoServer
