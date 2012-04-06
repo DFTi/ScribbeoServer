d865b4051ec934dbe72d1bf2d66b9d2b7f7a1b7e
Keyvan <keyvan@brevity>
2012-04-06 01:14:26 -0700

    refactor user validation. case insensitive login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,13 @@ class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation
   attr_accessor :password
   before_save :encrypt_password
-  
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :username
-  validates_uniqueness_of :username
+
+  validates :password, :confirmation=>true
+  validates :password, :presence=>true, :on=>:create
+  validates :username, :presence=>true, :uniqueness => {:case_sensitive => false}
   
   before_destroy { folders.clear }
   
-
   def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
@@ -46,7 +44,7 @@ class User < ActiveRecord::Base
   end
   
   def self.authenticate(username, password)
-    user = find_by_username(username)
+    user = where(["lower(username) = ?", username.downcase]).first
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
