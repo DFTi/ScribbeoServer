a5f1bb1afba173c6deb0f7db20232e8b6397b6a2
Keyvan Fatehi <keyvan@digitalfilmtree.com>
2013-04-15 04:17:10 -0700

    Removed vm_tools scripts. Now maintained at keyvanfatehi/DFT-Workflow

--- script/vm_tools/secure_tunnel.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-LOCAL_TUNNEL_PORT = ARGV[0]
-
-puts "Killing port-stealing ssh sessions on #{LOCAL_TUNNEL_PORT}"
-
-`ps aux | grep "ssh -L #{LOCAL_TUNNEL_PORT}" | awk '{print $2}'`.split.each do |pid|
-  `kill -9 #{pid} > /dev/null 2>&1`
-end
